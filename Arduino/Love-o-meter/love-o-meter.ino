06ed2c114441442823e2c802d8991296c5bc9494
Geoffrey <gvd8@hotmail.com>
2015-10-31 08:33:47 +0100

    Added schema & ppt for love-o-meter

--- Arduino/Love-o-meter/love-o-meter.ino
+++ Arduino/Love-o-meter/love-o-meter.ino
@@ -1,64 +1,65 @@
 /*
   Arduino Starter Kit example
- Project 3  - Love-O-Meter
+ Project 2  - Love-O-Meter
  
- This sketch is written to accompany Project 3 in the
- Arduino Starter Kit
+ This sketch is written to accompany Project 2 
  
  Parts required:
  1 TMP36 temperature sensor 
  3 red LEDs
  3 220 ohm resistors
  
- Created 13 September 2012
- by Scott Fitzgerald
+ Created 31 October 2015
+ by Geoffrey Vandiest
  
- http://arduino.cc/starterKit
- 
+  
  This example code is part of the public domain 
  */
 
 // named constant for the pin the sensor is connected to
 const int sensorPin = A0;
 // room temperature in Celcius
-float baselineTemp = 20.0;
+float baselineTemp = 0.0;
 
 void setup(){
   // open a serial connection to display values
   Serial.begin(9600);
+
   // set the LED pins as outputs
   // the for() loop saves some extra coding
   for(int pinNumber = 2; pinNumber<5; pinNumber++){
     pinMode(pinNumber,OUTPUT);
     digitalWrite(pinNumber, LOW);
   }
- 
+  
+  // retrieve the base temperature
   baselineTemp = getTemperature();
+  Serial.println("Base Temperature:");
+  Serial.print(baselineTemp);
 }
 
 
 void loop(){
 
+  // retrieve current temperature
   float temperature = getTemperature();
-  Serial.println(temperature);
+  Serial.println("Temperature:");
+  Serial.print(temperature);
 
   // if the temperature rises 3 degrees, turn an LED on 
  if(temperature >= baselineTemp+3 && temperature < baselineTemp+5){
-    setLights(HIGH, LOW, LOW);
-    Serial.println("A little!");
+    setLights(HIGH, LOW, LOW);  
   } // if the temperature rises 5-8 degrees, turn a second LED on  
   else if(temperature >= baselineTemp+5 && temperature < baselineTemp+8){
     setLights(HIGH, HIGH, LOW);
-    Serial.println("You're hot!");
   } // if the temperature rises more than 8 degrees, turn all LEDs on
   else if(temperature >= baselineTemp+8){
     setLights(HIGH, HIGH, HIGH);
-    Serial.println("In love!");
   }
-  else{ // if the current temperature is lower than the baseline
+  else { // if the current temperature is lower than the baseline
     setLights(LOW, LOW,LOW);
-    Serial.println("Cold!");
   }
+
   delay(1);
 }
 

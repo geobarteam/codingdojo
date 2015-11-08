8646eddb3d24886505230b2fbb0a5d3712669999
Geoffrey <gvd8@hotmail.com>
2015-11-08 12:47:43 +0100

    refactored

--- Arduino/Instrument/Step1/DistanceSynthetizer1/DistanceSynthetizer1.ino
+++ Arduino/Instrument/Step1/DistanceSynthetizer1/DistanceSynthetizer1.ino
@@ -7,15 +7,15 @@
  *
  * where the different tones are described as in the table:
  *
- * note 	frequency 	period 	timeHigh
- * c 	        261 Hz 	        3830 	1915 	
- * d 	        294 Hz 	        3400 	1700 	
- * e 	        329 Hz 	        3038 	1519 	
- * f 	        349 Hz 	        2864 	1432 	
- * g 	        392 Hz 	        2550 	1275 	
- * a 	        440 Hz 	        2272 	1136 	
- * b 	        493 Hz 	        2028	1014	
- * C	        523 Hz	        1912 	956
+ * note   frequency   period  timeHigh
+ * c          261 Hz          3830  1915  
+ * d          294 Hz          3400  1700  
+ * e          329 Hz          3038  1519  
+ * f          349 Hz          2864  1432  
+ * g          392 Hz          2550  1275  
+ * a          440 Hz          2272  1136  
+ * b          493 Hz          2028  1014  
+ * C          523 Hz          1912  956
  *
  * http://www.arduino.cc/en/Tutorial/Melody
  */
@@ -29,10 +29,39 @@ int length = 1; // the number of notes
 char notes[] = { 'c', 'd', 'e', 'f', 'g', 'a', 'b', 'C' }; // a space represents a rest
 int beats[] = { 1 };
 int tempo = 300;
-const float maxDistance = 2.0;
-const int PlayDuration = 200;
+
+float maxDistance = 2.0;
+int playDuration = 200;
+
+void setup() {
+  
+  Serial.begin(9600); // for debugging through serial monitor
+
+  // set pins mode
+  pinMode(speakerPin, OUTPUT);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+}
+
+void loop() {
+
+  // detect distance to object
+  float distance;
+  distance = getDistance();
+
+  // for debugging
+  Serial.println("distance:");
+  Serial.print(distance);
+
+  // transform distance into sound
+  play(distance);
+
+  // needed for smooth experience
+  delay(5);
+}
 
 void playTone(int tone, int duration) {
+
   for (long i = 0; i < duration * 1000L; i += tone * 2) {
     digitalWrite(speakerPin, HIGH);
     delayMicroseconds(tone);
@@ -42,6 +71,7 @@ void playTone(int tone, int duration) {
 }
 
 void playNote(char note, int duration) {
+
   char names[] = { 'c', 'd', 'e', 'f', 'g', 'a', 'b', 'C' };
   int tones[] = { 1915, 1700, 1519, 1432, 1275, 1136, 1014, 956 };
   
@@ -53,7 +83,7 @@ void playNote(char note, int duration) {
   }
 }
 
-void ping()
+void ping() // radar: send a short signal 
 {
   digitalWrite(trigPin, LOW); //Low high and low level take a short time to TrigPin pulse
   delayMicroseconds(2);
@@ -62,7 +92,7 @@ void ping()
   digitalWrite(trigPin, LOW);
 }
 
-float getDistance()
+float getDistance() // calculate the distance based on echo time of the radar
 {
   ping();
   float cm;
@@ -71,36 +101,31 @@ float getDistance()
   return cm;
 }
 
-void setup() {
-  Serial.begin(9600);
-  pinMode(speakerPin, OUTPUT);
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
-}
-
-void play(float distance){
+// play a note based on detected distance
+void play(float distance){ 
   
   float lowLimit;
   lowLimit = maxDistance;
 
-  Serial.println("distance:");
-  Serial.print(distance);
+  // if object is too far away do nothing
+  if (maxDistance < distance){
+    return;
+  }
 
+  // divide the maxdistance in 8 areas => 1 area = 1 note
+  float areaLenth = maxDistance/8;
+
+  // loop over each area 
   for (int i = 0; i < 8; i++) {
-    lowLimit = lowLimit - (maxDistance/8);
-    if (distance < maxDistance && distance >= lowLimit){
+    lowLimit = lowLimit - areaLenth;
+
+    // play the note if the object is in the area
+    if (distance < maxDistance && distance >= lowLimit){ 
       Serial.print(notes[i]);
-      playNote(notes[i], PlayDuration);
-      Serial.println("playnote:");
-      Serial.print(notes[i]);
+      playNote(notes[i], playDuration);
+      Serial.println("playnote:"); // for debugging
+      Serial.print(notes[i]); // for debugging
       return;
     }
   }
-}
-
-void loop() {
-  float distance;
-  distance = getDistance();
-  play(distance);
-  delay(5);
 }
